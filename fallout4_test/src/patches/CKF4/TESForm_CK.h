a0fea845b1a32248b6acf70f8186dc256dba4aa3
Perchik71 <vlad.shabalin.1992@mail.ru>
2020-10-21 12:13:00 +0300

    Active and FormID property
    Active and FormID property

--- fallout4_test/src/patches/CKF4/TESForm_CK.h
+++ fallout4_test/src/patches/CKF4/TESForm_CK.h
@@ -4,6 +4,16 @@
 
 class TESForm_CK
 {
+private:
+	char _pad[0x8];
+	uint32_t FormFlags;
+	uint32_t FormID;
+public:
+	inline bool GetActive(void) const { return (FormFlags & 2) != 0; }
+	inline uint32_t GetFormID() const { return FormID; }
+public:
+	__declspec(property(get = GetActive)) bool Active;
+	__declspec(property(get = GetFormID)) uint32_t FormID;
 public:
 	using Array = BSTArray<TESForm_CK *>;
 
