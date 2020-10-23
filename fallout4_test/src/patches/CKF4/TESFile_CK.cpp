32781bc036bc4b027db91b160144756d6a7b9bd2
Perchik71 <vlad.shabalin.1992@mail.ru>
2020-10-23 13:14:12 +0300

    Update

--- fallout4_test/src/patches/CKF4/TESFile_CK.cpp
+++ fallout4_test/src/patches/CKF4/TESFile_CK.cpp
@@ -1,12 +1,2 @@
 #include "../../common.h"
 #include "TESFile_CK.h"
-
-std::string TESFile_CK::GetFileName(void) const
-{
-	return m_FileName;
-}
-
-std::string TESFile_CK::GetFilePath(void) const
-{
-	return m_FilePath;
-}
--- fallout4_test/src/patches/CKF4/TESFile_CK.h
+++ fallout4_test/src/patches/CKF4/TESFile_CK.h
@@ -3,20 +3,36 @@
 #include <atomic>
 #include <string>
 
+#pragma pack(push, 1)
+
 class TESFile_CK
 {
 private:
 	TESFile_CK();
 	~TESFile_CK();
+public:
+	enum Type_t: uint32_t
+	{
+		FILE_RECORD_ESM			= 0x1,			// Master plugin
+		FILE_RECORD_CHECKED		= 0x4,			// Pending load/loaded
+		FILE_RECORD_ACTIVE		= 0x8,			// Save target
+		FILE_RECORD_LOCALIZED	= 0x80,			// Strings removed
+		FILE_RECORD_ESL			= 0x200,		// Small file
+	};
 private:
 	char _pad0[0x78];
 	char m_FileName[MAX_PATH];
 	char m_FilePath[MAX_PATH];
-	char _pad1[0x60];
+	//char _pad1[0x9];
+	//Type_t m_Type;
 public:
-	std::string GetFileName(void) const;
-	std::string GetFilePath(void) const;
+	std::string GetFileName(void) const { return m_FileName; }
+	std::string GetFilePath(void) const { return m_FilePath; }
+	//Type_t GetType(void) const { return m_Type; }
 public:
 	__declspec(property(get = GetFileName)) std::string FileName;
 	__declspec(property(get = GetFilePath)) std::string FilePath;
+	//__declspec(property(get = GetType)) Type_t Type;
 };
+
+#pragma pack(pop)
